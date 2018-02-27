bcf41e285a031e9923a9c191850a004eedcca359
Kenneth <krbrown@live.ca>
2018-02-27 19:47:00 +0000

    first 3 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,34 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# ActiveRecord::Schema.define do
+# 	create_table :stores
+# 	  t.string :name
+# 	  t.string :annual_revenue
+# 	  t.string :mens_apparel
+# 	  t.string :womens_apparel
+# 	end
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true
+  validates :annual_revenue, presence: true
+end
+
+Store.create!(
+	name: 'Burnaby',
+	annual_revenue: '3000000',
+	mens_apparel: true,
+	womens_apparel: true
+	)
+Store.create!(
+	name: 'Richmond',
+	annual_revenue: '1260000',
+	womens_apparel: true
+	)
+Store.create!(
+	name: 'Gastown',
+	annual_revenue: '190000',
+	mens_apparel: true
+	)
+
+puts Store.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store1.name = 'Target'
+@store1.save
+@store2 = Store.find(2)
+
+
